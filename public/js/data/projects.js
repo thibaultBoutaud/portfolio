101c73d47fc54a21439b0331cb7e9759c1824568
Thibault <thibault.boutaud@gmx.fr>
2025-05-10 18:55:13 +0200

    update data projets

--- public/js/data/projects.js
+++ public/js/data/projects.js
@@ -1,6 +1,24 @@
 export const projects = [
-    {
+        {
         id: 1,
+        name: "Portfolio",
+        description: "Portfolio présentant mes projets personnels",
+        img: "portfolio.png",
+        techno_logos: ["js.png", "poo.png"],
+        isOnline: true,
+        github_url: "https://github.com/thibaultBoutaud/portfolio.git",
+        site_url: "https://portfoliotb.netlify.app/",
+        fonctionalities: [
+            "Projet en JS (Poo) et css (BEM)",
+            "SPA sans SSR en POO SOLID",
+            "Navigation avec l'api history",
+            "Routes mix entre modèle REST et searchParams",
+            "SEO dynamique",
+            "Portfolio qui présente mes projets"
+        ]
+    },
+    {
+        id: 2,
         name: "App culinaire",
         description: "Application pour recettes de cuisine",
         img: "cuisine_img.png",
@@ -21,7 +39,7 @@ export const projects = [
         github_img: ["cuisine_1.png", "cuisine_2.png", "cuisine_3.png"]
     },
     {
-        id: 2,
+        id: 3,
         name: "Chat Msn",
         description: "Réseau social avec messagerie instantanée",
         img: "msn_img.png",
@@ -41,7 +59,7 @@ export const projects = [
         github_img: ["msn_1.png", "msn_2.png", "msn_3.png", "msn_4.png", "msn_5.png"]
     },
     {
-        id: 3,
+        id: 4,
         name: "App e-commerce",
         description: "Application pour gestion de produits en ligne",
         img: "trendtangle.png",
@@ -63,7 +81,7 @@ export const projects = [
         github_img: ["trend_1.png", "trend_2.png", "trend_3.png", "trend_4.png", "trend_5.png", "trend_6.png", "trend_7.png", "trend_8.png"]
     },
     {
-        id: 4,
+        id: 5,
         name: "Gestionnaire de révision",
         description: "Application pour organiser des révisions selon la répétition espacée",
         img: "boxRevision.png",
@@ -83,20 +101,7 @@ export const projects = [
         ],
          github_img: ["revi_0.png", "revi_1.png", "revi_2.png", "revi_3.png"]
     },
-    {
-        id: 5,
-        name: "Portfolio",
-        description: "Portfolio présentant mes projets personnels",
-        img: "portfolio.png",
-        techno_logos: ["js.png", "poo.png"],
-        isOnline: true,
-        github_url: "https://github.com/thibaultBoutaud/portfolio.git",
-        site_url: "https://portfoliotb.netlify.app/",
-        fonctionalities: [
-            "Projet en JS (Poo) et css (BEM)",
-            "Portfolio qui présente mes projets"
-        ]
-    },
+
     {
         id: 6,
         name: "To-Do-List Agenda",
