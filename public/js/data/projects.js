98fdfb5c14be638c47148b162c3892db9ae8b034
Thibault <thibault.boutaud@gmx.fr>
2025-08-16 09:20:37 +0200

    ajout projet antiquité

--- public/js/data/projects.js
+++ public/js/data/projects.js
@@ -1,6 +1,44 @@
 export const projects = [
     {
         id: 1,
+        name: "Agenda Hôtelier",
+        description: "Application hôtelière avec agenda et plannings",
+        img: "hotel_accueil.png",
+        techno_logos: ["js.png", "poo.png"],
+        isOnline: false,
+        github_url: "https://github.com/thibaultBoutaud/hotel-manager.git",
+        site_url: "",
+        fonctionalities: [
+            "Projet en JS (Poo) et css (BEM) et sql (mySql)",
+            "SPA sans SSR en POO SOLID",
+            "Navigation avec l'api history",
+            "Routes mix entre modèle REST et searchParams",
+            "SEO dynamique",
+            "Agenda type google calendar",
+            "Plannings"
+        ],
+        github_img: ["hotel_1.png", "hotel_2.png", "hotel_3.png", "hotel_4.png", "hotel_5.png", "hotel_6.png", "hotel_7.png", "hotel_8.png"]
+    },
+    {
+        id: 2,
+        name: "Site web d'antiquaire",
+        description: "Site web autogérable qui présente la galerie d'un antiquaire ainsi que des articles de journaux",
+        img: "antiq.png",
+        techno_logos: ["js.png", "react.png", "mySql.png"],
+        isOnline: true,
+        github_url: "https://github.com/thibaultBoutaud/antiq.git",
+        site_url: "https://tbuilder.fr",
+        fonctionalities: [
+            "Projet en React et mySql",
+            "carouselle d'images", 
+            "RESTful route",
+            "Service de mailling",
+            "Auto-création de fiche objets et d'articles"
+        ],
+        github_img: ["antiq_1.png", "antiq_2.png", "antiq_3.png", "antiq_4.png", "antiq_5.png", "antiq_6.png", "antiq_7.png"]
+    },
+    {
+        id: 3,
         name: "Portfolio",
         description: "Portfolio présentant mes projets personnels",
         img: "portfolio.png",
@@ -18,7 +56,7 @@ export const projects = [
         ]
     },
     {
-        id: 2,
+        id: 4,
         name: "App culinaire",
         description: "Application pour recettes de cuisine",
         img: "cuisine_img.png",
@@ -39,7 +77,7 @@ export const projects = [
         github_img: ["cuisine_1.png", "cuisine_2.png", "cuisine_3.png"]
     },
     {
-        id: 3,
+        id: 5,
         name: "Chat Msn",
         description: "Réseau social avec messagerie instantanée",
         img: "msn_img.png",
@@ -59,7 +97,7 @@ export const projects = [
         github_img: ["msn_1.png", "msn_2.png", "msn_3.png", "msn_4.png", "msn_5.png"]
     },
     {
-        id: 4,
+        id: 6,
         name: "App e-commerce",
         description: "Application pour gestion de produits en ligne",
         img: "trendtangle.png",
@@ -81,7 +119,7 @@ export const projects = [
         github_img: ["trend_1.png", "trend_2.png", "trend_3.png", "trend_4.png", "trend_5.png", "trend_6.png", "trend_7.png", "trend_8.png"]
     },
     {
-        id: 5,
+        id: 7,
         name: "Gestionnaire de révision",
         description: "Application pour organiser des révisions selon la répétition espacée",
         img: "boxRevision.png",
